a5287cc8f6345d776c00ae26268540c3c0683bc8
Gord Tanner <gtanner@gmail.com>
2013-10-31 21:26:21 -0400

    Removed chrome extension building
    Cleaned up lint and removed the extension building. Also removed the
    cleanBody stuff as that only matters in the extension since we don't
    inject ripple into the current document in hosted.

--- build/build.js
+++ build/build.js
@@ -25,7 +25,6 @@ var fs = require('fs'),
     clean = require('./clean'),
     _c = require('./conf'),
     compress = require('./compress'),
-    chromeExt = require('./targets/chrome.extension'),
     hosted = require('./targets/hosted');
 
 function _done(error) {
@@ -54,19 +53,8 @@ module.exports = _handle(function (ext, opts) {
     opts = opts || {};
 
     var build = jWorkflow.order(clean)
-                         .andThen(pack);
-
-    switch (ext) {
-    case 'chrome.extension':
-        build.andThen(chromeExt);
-        break;
-    case 'hosted':
-        build.andThen(hosted);
-        break;
-    default:
-        build.andThen(chromeExt)
-             .andThen(hosted);
-    }
+                         .andThen(pack)
+                         .andThen(hosted);
 
     if (opts.compress) {
         build.andThen(compress);
